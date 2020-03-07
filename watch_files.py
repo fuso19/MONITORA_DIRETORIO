bae5b2f2c8b03e2a42c542585da9627d0894c0f5
Pedro Damassceno <pedro.damasceno@hotmail.com>
2020-03-07 17:51:46 -0300

    First Commit

---
+++ watch_files.py
@@ -0,0 +1,34 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+import json
+import time
+
+
+# Classe que será notificado quando houver mudanças no diretório
+class MyHandler(FileSystemEventHandler):
+    i = 1
+
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + '/' + filename
+            new_destination = folder_destination + '/' + filename
+            os.rename(src, new_destination)
+
+
+folder_to_track = "./FROM"
+folder_destination = './TO'
+
+event_handler = MyHandler()
+
+# Objeto que monitora o sistema
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+    observer.join()
